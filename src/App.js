98739b9060c26f561cd3a10034d9b7844bd79ecb
Eorium <travisgent312@gmail.com>
2020-06-19 13:34:13 -0600

    add finished webpage with pizza order page and home page navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
 import "./App.css";
 import Home from "./Home";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import PizzaOrder from "./PizzaOrder";
 
 const App = () => {
   return (
     <div className="App">
-      <Home />
+      <BrowserRouter>
+        <nav className="navBar">
+          <h1>Lambda Eats</h1>
+          <Link to="/" className="button">Home</Link>
+          <Link to="/PizzaOrder" className="button">Get Pizza!</Link>
+        </nav>
+        <Route exact path="/" component={Home} />
+        <Route path="/PizzaOrder" component={PizzaOrder} />
+      </BrowserRouter>
     </div>
   );
 };
--- src/PizzaOrder.js
+++ src/PizzaOrder.js
@@ -1,10 +1,156 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import * as yup from "yup";
 
 const PizzaOrder = () => {
+  const formSchema = yup.object().shape({
+    name: yup.string().required("Name is a required field")
+  });
+
+  const [allMyData, setAllMyData] = useState({
+    name: "",
+    size: "Small",
+    pepperoni: false,
+    olives: false,
+    bacon: false,
+    pepperoncini: false,
+    special: ""
+  })
+
+  const [errors, setErrors] = useState({
+    name: "",
+  });
+
+  const validate = (event) => {
+    yup.reach(formSchema, event.target.name)
+    .validate(event.target.value)
+    .then( valid => {
+      setErrors({
+        ...errors,
+        [event.target.name]: ""
+      })
+    })
+    .catch( err => {
+      console.log(err.errors);
+      setErrors({
+        ...errors,
+        [event.target.name]: err.errors[0]
+      })
+    })
+  };
+
+  const [orderPizza, setOrderPizza] = useState({
+    name: "",
+    size: "Small",
+    pepperoni: false,
+    olives: false,
+    bacon: false,
+    pepperoncini: false,
+    special: ""
+  })
+
+  const inputChange = (event) => {
+    event.persist()
+    validate(event);
+    let value = event.target.type === "checkbox" ? event.target.checked : event.target.value;
+    setOrderPizza({
+      ...orderPizza,
+      [event.target.name]: value
+    });
+  }
+
+  const formSubmit = (event) => {
+    event.preventDefault();
+    setAllMyData(orderPizza)
+    console.log(allMyData)
+  };
+
   return (
-    <div>
-      Hello!
+    <div className="options">
+      <div className="pizzaHeader" />
+      <form onSubmit={formSubmit}> 
+        <label htmlFor="name">Name:
+          <input 
+            id="name"
+            name="name"
+            type="text" 
+            placeholder="Enter Name" 
+            value={orderPizza.name}
+            onChange={inputChange}
+          />
+          {errors.name.length > 0 ? <p className="error">{errors.name}</p> : null}
+        </label>
+        <br />
+        <label htmlFor="size">Select Pizza Size:
+          <select
+            id="size"
+            name="size"
+            value={orderPizza.size}
+            onChange={inputChange}>
+              <option value="Small">Small</option>
+              <option value="Medium">Medium</option>
+              <option value="Large">Large</option>
+          </select>
+        </label>
+
+        <h2>Select toppings that you want:</h2>
+
+        <label htmlFor="pepperoni">Pepperoni:
+          <input 
+            id="pepperoni"
+            name="pepperoni"
+            type="checkbox"
+            checked={orderPizza.pepperoni}
+            onChange={inputChange}
+          />
+        </label>
+
+        <label htmlFor="olives">Olives:
+          <input 
+            id="olives"
+            name="olives"
+            type="checkbox"
+            checked={orderPizza.olives}
+            onChange={inputChange}
+          />
+        </label>
+
+        <label htmlFor="bacon">Bacon:
+          <input 
+            id="bacon"
+            name="bacon"
+            type="checkbox"
+            checked={orderPizza.bacon}
+            onChange={inputChange}
+          />
+        </label>
+
+        <label htmlFor="pepperoncini">Pepperoncini:
+          <input 
+            id="pepperoncini"
+            name="pepperoncini"
+            type="checkbox"
+            checked={orderPizza.pepperoncini}
+            onChange={inputChange}
+          />
+        </label>
+
+        <h2>Anything we should know:</h2>
+
+        <label htmlFor="special">
+          <textarea 
+            id="special"
+            name="special"
+            rows="5"
+            cols="50" 
+            placeholder="Enter Here" 
+            value={orderPizza.special}
+            onChange={inputChange}
+          />
+        </label>
+
+        <br /><button type="submit" className="Submit">Submit Order</button>
+      </form>
     </div>
   );
 };
